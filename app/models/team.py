9c93cff5ba9f13806fc38e163f070258b5dcebb8
Ivan <csmain2@mail.ru>
2026-04-07 11:24:01 +0300

    remove unused import

--- app/models/team.py
+++ app/models/team.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, Integer
 from app.db.base_class import Base
 
 
